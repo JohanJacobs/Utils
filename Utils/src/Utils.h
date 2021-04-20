c308d76012829e85dfebf4217e0cc85868a89bae
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2021-04-20 19:08:58 +1200

    setup

---
+++ Utils/src/Utils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+namespace UTILS
+{
+	class Utils
+	{
+	public:
+		void print() 
+		{
+			std::cout << "Hello Utils\n";
+		}
+	};
+}
---
+++ sandbox/src/Sandbox.cpp
@@ -0,0 +1,8 @@
+#include "Utils.h"
+int main()
+{
+	UTILS::Utils u;
+	u.print();
+
+	return 0;
+}
